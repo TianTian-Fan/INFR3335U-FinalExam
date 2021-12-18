fd90fb528eff2525944a131549df5214a5dcdeaf
Carl <slamacowcrea@gmail.com>
2021-12-18 08:02:07 -0500

    FinalExam

--- Assets/Scripts/TouchMovement.cs
+++ Assets/Scripts/TouchMovement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class TouchMovement : MonoBehaviour
 {
     public FixedJoystick joystick;
+    public FixedJoystick lookJoystick;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +19,9 @@ public class TouchMovement : MonoBehaviour
         float ver = joystick.Vertical;
 
         Vector3 direction = new Vector3(hoz, 0, ver).normalized;
-        transform.Translate(direction * 0.02f, Space.World);
+        transform.Translate(direction * 0.005f, Space.World);
+
+        float rotateHoz = lookJoystick.Horizontal * 0.1f;
+        transform.Rotate(0, rotateHoz, 0);
     }
 }
